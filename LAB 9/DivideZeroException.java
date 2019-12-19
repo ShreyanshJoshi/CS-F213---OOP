cedb6bbcc33a2a2de52dc382dbc2fe02d27c38b1
Shreyansh Joshi <shreyanshjoshi13@gmail.com>
2019-12-19 10:34:05 +0530

    Delete DivideZeroException.java

--- LAB 9/DivideZeroException.java
+++
@@ -1,5 +0,0 @@
-class DivideZeroException extends java.lang.Exception {
-	public DivideZeroException​(String type) {
-		super("Cannot calculate average cost because number of "+type+" are insufficient");
-	}
-}
