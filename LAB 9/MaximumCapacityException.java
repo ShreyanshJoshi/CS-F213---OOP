2aa59d65928bcd68315722c01ad0b08c17a276e9
Shreyansh Joshi <shreyanshjoshi13@gmail.com>
2019-12-19 10:34:57 +0530

    Delete MaximumCapacityException.java

--- LAB 9/MaximumCapacityException.java
+++
@@ -1,5 +0,0 @@
-public class MaximumCapacityException extends java.lang.Exception {
-	public MaximumCapacityException​(String type, String name) {
-		super(type+ " capacity limit reached in "+name);
-	}	
-}
