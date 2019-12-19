0be8beba75a25bea3ab96e9024972a555c3dc4a8
Shreyansh Joshi <shreyanshjoshi13@gmail.com>
2019-12-19 10:35:17 +0530

    Delete invalidDeviceException.java

--- LAB 9/invalidDeviceException.java
+++
@@ -1,5 +0,0 @@
-public class invalidDeviceException extends java.lang.Exception {
-	public invalidDeviceException​(String roomType,String appliance) {
-		super(roomType+" cannot have appliance "+appliance);
-	}
-}
