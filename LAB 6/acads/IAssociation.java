bc15b619cfcce27c1e2b520ee5af85af6986e91a
Shreyansh Joshi <shreyanshjoshi13@gmail.com>
2019-12-19 10:44:29 +0530

    Delete IAssociation.java

--- LAB 6/acads/IAssociation.java
+++
@@ -1,7 +0,0 @@
-package acads;
-import extracurriculars.Organization;
-public interface IAssociation extends Organization {
-	static final int BASE_BUDGET=20000;
-
-	int calculateProjectBudget();
-}
